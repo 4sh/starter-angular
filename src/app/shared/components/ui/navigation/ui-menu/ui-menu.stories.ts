b8b6d5cc3453bd49c71d345223f56ac0d54465b1
Robin Lopez <robinlopez.contact@gmail.com>
2026-08-05 15:23:48 +0200

    docs(ui-menu): Link page figma

--- src/app/shared/components/ui/navigation/ui-menu/ui-menu.stories.ts
+++ src/app/shared/components/ui/navigation/ui-menu/ui-menu.stories.ts
@@ -23,7 +23,7 @@ const meta: Meta<UiMenu> = {
     layout: 'padded',
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/design/GZww5hdUA49LB8XWeWP6tl/-Projet----UI-Kit?node-id=0-1&t=PXOwFotKvf72dn1c-1',
+      url: 'https://www.figma.com/design/GZww5hdUA49LB8XWeWP6tl/-Projet----UI-Kit?node-id=3094-10720&t=frNskMgWPnG5cChx-1',
     },
   },
   argTypes: {
