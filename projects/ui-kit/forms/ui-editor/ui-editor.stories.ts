69e3bdef5e19c68abba0023eb7d39054ed42c756
Robin Lopez <robinlopez.contact@gmail.com>
2026-08-27 14:41:26 +0200

    doc(ui-editor): Add link Figma

--- projects/ui-kit/forms/ui-editor/ui-editor.stories.ts
+++ projects/ui-kit/forms/ui-editor/ui-editor.stories.ts
@@ -13,7 +13,7 @@ const meta: Meta<UiEditor> = {
     layout: 'centered',
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/design/GZww5hdUA49LB8XWeWP6tl/-Projet----UI-Kit?node-id=0-1&t=PXOwFotKvf72dn1c-1',
+      url: 'https://www.figma.com/design/GZww5hdUA49LB8XWeWP6tl/-Projet----UI-Kit?node-id=3691-39299',
     },
   },
   argTypes: {
